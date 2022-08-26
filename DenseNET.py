04e6f7de137f8a74d21f5dd6bf8ee6b2a8bc6add
ILoveFatCats <105158865+ILoveFatCats@users.noreply.github.com>
2022-08-26 14:29:14 +0200

    Update DenseNET.py

--- DenseNET.py
+++ DenseNET.py
@@ -32,6 +32,10 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 # tf.debugging.set_log_device_placement(True)
 
+# Setting up the distributed training
+
+mirrored_strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
+
 # Create some tensors
 a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
